21681a0b76339dd4e4b0e2ef49994f765c652f13
Giovanne Russo <russogiovanne@gmail.com>
2021-07-27 14:20:11 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+public class Piece {
+    protected Position position; // Não é a posição no tabuleiro, mas na matriz
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null; // Opcional
+    }
+
+    protected Board getBoard() { //O tabuleiro só pode ser acessado dentro do pacote e das subclasses de Peças
+        return board;
+    }
+
+
+}
