e467fdc8eca61b0282d9df64970114e7c27671b1
Giovanne Russo <russogiovanne@gmail.com>
2021-08-03 17:21:13 -0300

    Special move: En Passant

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Pawn extends ChessPiece {
 
-    public Pawn(Board board, Color color) {
+    private ChessMatch chessMatch;
+
+    public Pawn(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     @Override
@@ -40,6 +44,21 @@ public class Pawn extends ChessPiece {
             if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
+            //En Passant
+            if (position.getRow() == 3) {
+                //En Passant à esquerda
+                Position left = new Position(position.getRow(), position.getColumn() - 1 );
+                //Teste verificando se a Peça existe, se é um Oponente e se está vulnerável a um En Passant
+                if(getBoard().positionExists(left) && isThereOpponentPiece(left) && getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
+                    mat[left.getRow() - 1][left.getColumn()] = true;
+                }
+                //En Passant à direita
+                Position right = new Position(position.getRow(), position.getColumn() + 1 );
+                //Teste verificando se a Peça existe, se é um Oponente e se está vulnerável a um En Passant
+                if(getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
+                    mat[right.getRow() - 1][right.getColumn()] = true;
+                }
+            }
         }
         else { //Cor Preta
             p.setValues(position.getRow() + 1, position.getColumn()); //Abaixo do peão preto
@@ -59,6 +78,21 @@ public class Pawn extends ChessPiece {
             p.setValues(position.getRow() + 1, position.getColumn() + 1); //Diagonal inferior direita
             if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
+            }
+            //En Passant
+            if (position.getRow() == 4) {
+                //En Passant à esquerda
+                Position left = new Position(position.getRow(), position.getColumn() - 1 );
+                //Teste verificando se a Peça existe, se é um Oponente e se está vulnerável a um En Passant
+                if(getBoard().positionExists(left) && isThereOpponentPiece(left) && getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
+                    mat[left.getRow() + 1][left.getColumn()] = true;
+                }
+                //En Passant à direita
+                Position right = new Position(position.getRow(), position.getColumn() + 1 );
+                //Teste verificando se a Peça existe, se é um Oponente e se está vulnerável a um En Passant
+                if(getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
+                    mat[right.getRow() + 1][right.getColumn()] = true;
+                }
             }
         }
         return mat;
